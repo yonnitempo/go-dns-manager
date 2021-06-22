dd7df434318745afa793430c257332e75cc8d201
Alvaro Retortillo <alvaroretortillo@gmail.com>
2021-06-22 15:09:13 +0000

    Some authentication working.

--- server.go
+++ server.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Server struct {
-	config []Config
+	config map[string]Config
 }
 
 type Config struct {
-	Domain string	`json:"domain"`
-	Secret string	`json:"secret"`
+	Domain string `json:"domain"`
+	Secret string `json:"secret"`
 }
 
 func (server *Server) calculate_sha1(secret string) string {
@@ -37,12 +37,19 @@ func (server *Server) load_configuration(path string) {
 	}
 }
 
-func (server *Server) manage_secret(writer http.ResponseWriter, secret_values []string) {
-	if len(secret_values) == 1 {
+func (server *Server) manage_secret(writer http.ResponseWriter, secret_values []string, domain_values []string) {
+	if len(secret_values) == 1 && len(domain_values) == 1 {
 		log.Println("Secret is", secret_values[0])
+		log.Println("Expected secret for domain is", server.config[domain_values[0]].Secret)
 		secret := secret_values[0]
-		sha1_hex := server.calculate_sha1(secret)
-		fmt.Fprintf(writer, "SHA1: %s\n", sha1_hex)
+		sha1_string := server.calculate_sha1(secret)
+		if sha1_string == server.config[domain_values[0]].Secret {
+			log.Printf("Secrets match for domain: %s.\n", domain_values[0])
+			fmt.Fprintf(writer, "Authenticated\n")
+		} else {
+			log.Printf("Secrets do not match for domain: %s.\n", domain_values[0])
+			http.Error(writer, "Secret does not match!", http.StatusForbidden)
+		}
 	} else {
 		log.Println("Secret not properly set!")
 		http.Error(writer, "Secret not properly set!!", http.StatusForbidden)
@@ -52,9 +59,11 @@ func (server *Server) manage_secret(writer http.ResponseWriter, secret_values []
 
 func (server *Server) dns_updater(writer http.ResponseWriter, request *http.Request) {
 	query_keys := request.URL.Query()
-	if value, ok := query_keys["secret"]; ok {
-		log.Println("There is a secret on query. Value:", value)
-		server.manage_secret(writer, value)
+	if secret_values, ok := query_keys["secret"]; ok {
+		if domain_values, ok := query_keys["domain"]; ok {
+			log.Printf("There is a secret and domain on query: %s - %s", secret_values, domain_values)
+			server.manage_secret(writer, secret_values, domain_values)
+		}
 	}
 }
 
