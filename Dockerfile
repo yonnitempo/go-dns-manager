73feec519a063f770952757cd941972488e51f8f
Alvaro Retortillo <alvaroretortillo@gmail.com>
2021-06-28 21:33:05 +0000

    Added code to update DNS record on google cloud.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM arm32v6/alpine:3.12
 # GET THIS FROM: https://github.com/docker-library/python/tree/master/3.9-rc/alpine3.12
 
 RUN apk add --no-cache musl-dev go git
-run go get fmt net/http log crypto/sha1 encoding/json io/ioutil net time google.golang.org/api/dns/v1
+run go get fmt net/http log crypto/sha1 encoding/json io/ioutil net time google.golang.org/api/dns/v1 google.golang.org/api/option
 workdir /repo/
 copy *.go /repo/
 run go build -o /bin/server
--- server.go
+++ server.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"context"
 	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	"google.golang.org/api/dns/v1"
+	"google.golang.org/api/option"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"strings"
-	"time"
 )
 
+const ResourceRecordSetTypeA = "A"
+
 type Server struct {
-	config map[string]Config
+	config             map[string]Config
+	zoneName           string
+	projectName        string
+	serviceAccountFile string
 }
 
 type Config struct {
@@ -27,6 +34,63 @@ func (server *Server) calculate_sha1(secret string) string {
 	// echo -n $secret|sha1sum
 	sha1_hex := sha1_object.Sum(nil)
 	return fmt.Sprintf("%x", sha1_hex)
+}
+
+func (server *Server) getDnsRecord(dnsService *dns.Service, domain string) (*dns.ResourceRecordSetsListResponse, error) {
+	log.Printf("Project: %s, Zone: %s. Domain: %s", server.projectName, server.zoneName, domain)
+	record, err := dnsService.ResourceRecordSets.List(server.projectName, server.zoneName).Name(domain).Type(ResourceRecordSetTypeA).Do()
+
+	if err != nil {
+		return nil, fmt.Errorf("Issues find record %s: %s", domain, err)
+	}
+
+	return record, nil
+}
+
+func (server *Server) updateDnsRecord(dnsService *dns.Service, record *dns.ResourceRecordSet, newIPAddress string) error {
+	change := &dns.Change{
+		Deletions: []*dns.ResourceRecordSet{record},
+		Additions: []*dns.ResourceRecordSet{
+			{
+				Name:    record.Name,
+				Type:    record.Type,
+				Ttl:     record.Ttl,
+				Rrdatas: []string{newIPAddress},
+			},
+		},
+	}
+
+	log.Printf("Updating A record for %s - from: %v to: %v\n", record.Name, change.Deletions[0].Rrdatas, change.Additions[0].Rrdatas)
+
+	_, err := dnsService.Changes.Create(server.projectName, server.zoneName, change).Do()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (server *Server) manageUpdateDNSRecord(domain string, NewIPAddress string) error {
+	log.Printf("Updating %s with new IP address %s.\n", domain, NewIPAddress)
+	ctx := context.Background()
+	dnsService, err := dns.NewService(ctx, option.WithCredentialsFile(server.serviceAccountFile))
+	if err != nil {
+		fmt.Errorf("Cannot get DNS Service: %s", err)
+	}
+	record, err := server.getDnsRecord(dnsService, domain)
+	if err != nil {
+		fmt.Errorf("Cannot get record: %s", err)
+	}
+	if record == nil {
+		fmt.Errorf("record is nil :(")
+	}
+	// s, _ := record.MarshalJSON()
+	log.Printf("Record: %s.", record)
+
+	server.updateDnsRecord(dnsService, record.Rrsets[0], NewIPAddress)
+	if err != nil {
+		fmt.Errorf("Error updating DNS A record: %s", err)
+	}
+	log.Println("Updated.")
+	return nil
 }
 
 func (server *Server) load_configuration(path string) {
@@ -45,17 +109,11 @@ func (server *Server) manage_update_dns_a_record(writer http.ResponseWriter, req
 	if server.is_dns_record_out_of_date(ip_address, domain) {
 		log.Printf("Updating A record for %s with IP %s\n", domain, ip_address)
 		fmt.Fprintf(writer, "Updating A record…\n")
-		server.update_a_record_for_domain(domain, ip_address)
+		server.manageUpdateDNSRecord(domain, ip_address)
 	} else {
 		log.Printf("Already up to date A record for %s with IP %s.\n", domain, ip_address)
 		fmt.Fprintf(writer, "A record up to date.\n")
 	}
-}
-
-func (server *Server) update_a_record_for_domain(domain string, ipaddr string) {
-	log.Println("Updating DNS Record on Google Cloud…")
-	time.Sleep(3 * time.Second)
-	log.Println("Updated DNS Record on Google Cloud.")
 }
 
 func (server *Server) is_dns_record_out_of_date(ip_address string, domain string) bool {
@@ -111,6 +169,9 @@ func (server *Server) dns_updater(writer http.ResponseWriter, request *http.Requ
 func main() {
 	server := Server{}
 	server.load_configuration("config.json")
+	server.zoneName = "[INSERT HERE PROJECT]"
+	server.projectName = "[INSERT HERE ZONE]"
+	server.serviceAccountFile = "api-key.json"
 	http.HandleFunc("/dns_updater", server.dns_updater)
 
 	log.Println("Listening for incomming connections…")
